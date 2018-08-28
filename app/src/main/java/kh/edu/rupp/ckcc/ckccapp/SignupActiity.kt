aa8c4eff2672ccbaaac62ee708fc208cc498bd55
leapkh <leapkh@yahoo.com>
2018-08-28 19:57:27 +0700

    Add signup feature

---
+++ app/src/main/java/kh/edu/rupp/ckcc/ckccapp/SignupActiity.kt
@@ -0,0 +1,12 @@
+package kh.edu.rupp.ckcc.ckccapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+
+    fun signup(){
+
+    }
+
+}
